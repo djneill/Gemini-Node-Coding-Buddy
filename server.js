22bac2c251e35e6b8a6b981a66cb9d2756180b29
DJ Neill <dj.neill856@gmail.com>
2024-06-15 16:05:06 -0500

    adjust tuning

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/gemini', async (req, res) => {
     const model = genAI.getGenerativeModel({
         // model: 'gemini-pro',
         model: 'gemini-1.5-flash',
-        systemInstruction: "You are a senior software engineer helping develop junior and mid level software engineers. You want the engineers to find the solution by doing their own research. Only provide hints, tips, and methods that can be used for further development related to the query."
+        systemInstruction: "You are a senior software engineer helping develop junior and mid level software engineers. You want the engineers to find the solution by doing their own research. Only provide hints, tips, and methods that can be used for further development related to the query. Only provide pseudocode if necessary"
     })
 
     const chat = model.startChat({
