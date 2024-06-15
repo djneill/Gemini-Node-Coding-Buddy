5b2c97c7779e5cc96746195745333ee32c6c08da
DJ Neill <dj.neill856@gmail.com>
2024-06-15 15:48:16 -0500

    serve dist file

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
+const genAI = new GoogleGenerativeAI(process.env.GEM_API)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'dist')))
 
-import { GoogleGenerativeAI } from '@google/generative-ai'
-
-const genAI = new GoogleGenerativeAI(process.env.GEM_API)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.post('/gemini', async (req, res) => {
     const model = genAI.getGenerativeModel({
